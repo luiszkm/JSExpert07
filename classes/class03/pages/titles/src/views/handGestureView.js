449e20c0359b476a25ec2105d5e166272d791cc0
luiszkm <luiszkm@gmail.com>
2023-03-01 16:23:08 -0300

    create class3

---
+++ classes/class03/pages/titles/src/views/handGestureView.js
@@ -0,0 +1,17 @@
+
+
+export default class HandGestureView {
+
+  loop(fn){
+    requestAnimationFrame(fn)
+  }
+
+  scrollPage(top){
+    scroll({
+      top,
+      behavior: "smooth"
+    })
+  }
+
+
+}
